25189063594ed728d14eea427f44e17828629939
vrunoa <vruno.alassia@gmail.com>
2015-08-28 17:49:49 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo_fail()
+{
+  echo -e "\033[31m✗ \033[0m${1}";
+  exit 1
+}
+echo_ok()
+{
+  echo -e "\033[32m✓ \033[0m${1}";
+}
+
+# clean
+if [ -f linux-scanner ]; 
+then
+  rm linux-scanner
+fi
+
+ARCH=$(uname -m)
+if [ $ARCH = "x86_64" ];
+then
+  FLAGS="-L/usr/lib/x86_64-linux-gnu/ " 
+else
+  FLAGS="-L/usr/lib/i386-linux-gnu/ "
+fi
+FLAGS+="-I/usr/include "
+FLAGS+="-Wall "
+FLAGS+="-Wno-write-strings "
+
+INPUT="main.cpp "
+OUTPUT="-o linux-scanner "
+
+BUILD=$(gcc $FLAGS $OUTPUT $INPUT -lsane)
+
+if [ -f linux-scanner ];
+then
+  chmod 755 linux-scanner
+  echo_ok "linux-scanner cli successfully created"
+else
+  echo_fail "Error building linux-scanner cli"
+fi
