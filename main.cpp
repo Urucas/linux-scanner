a67dc1a9998b1d5304c50f6b9e1e43bd7d9d6047
vrunoa <vruno.alassia@gmail.com>
2015-08-28 20:39:12 +0000

    trying to start & open scanner device

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ class LinuxScanner {
     int scan();
     int prepare();
     static void authCallback(SANE_String_Const c, SANE_Char user[SANE_MAX_USERNAME_LEN], SANE_Char passi[SANE_MAX_PASSWORD_LEN]);
-    const SANE_Device **device_list;
 
   protected:
     char* imagePath;
     void log(char* msg);
-    SANE_Device selected_device;
+    void log(char* msg, SANE_Status status);
+    const SANE_Device* selected_device;
+    bool isGoood(SANE_Status status);
 };
 
 void LinuxScanner::authCallback(SANE_String_Const resource, SANE_Char* username, SANE_Char* password) {
@@ -28,26 +29,28 @@ void LinuxScanner::log(char* msg) {
   printf("[%s]\n", msg);
 }
 
+void LinuxScanner::log(char* msg, SANE_Status status) {
+  printf("[%s]\n", msg);
+  printf("[%s]\n", sane_strstatus(status));
+}
+
 int LinuxScanner::prepare() {
   this->log("Initializing sane");
   SANE_Int version[1] = {SANE_CURRENT_MAJOR};
   SANE_Status status = sane_init(version, &LinuxScanner::authCallback);
-  
-  // SANE_String_Const strstatus = sane_strstatus(status);
-  if(status != SANE_STATUS_GOOD) {
-    this->log("Error initializing SANE");
+ 
+  if(!this->isGoood(status)) {
+    this->log("Error initializing SANE", status);
     return 1;
   }
-
+  
+  const SANE_Device **device_list;
   this->log("Getting available devices");
   status = sane_get_devices(&device_list, SANE_TRUE);
-  if(status != SANE_STATUS_GOOD) {
-    this->log("[Error getting devices list]\n");
+  if(!this->isGoood(status)) {
+    this->log("[Error getting devices list]\n", status);
     return 1;
   }
-  // Testing device
-  // Device ready EPSON NX TX SX100/ME300/PX-401A
-  
   int i = 0;
   for (; device_list[i]; ++i) {
     printf ("device `%s' is a %s %s %s\n",                                 device_list[i]->name, device_list[i]->vendor,                              device_list[i]->model, device_list[i]->type);
@@ -56,10 +59,38 @@ int LinuxScanner::prepare() {
     this->log("No devices available");
     return 1;
   }
+  this->selected_device = device_list[0];
   return 0;
 }
 
+bool LinuxScanner::isGoood(SANE_Status status) {
+  if(status != SANE_STATUS_GOOD) {
+    return false;
+  }
+  return true;
+}
+
 int LinuxScanner::scan() {
+
+  this->log("Trying to open device");
+  printf("%s", this->selected_device->name);
+  static SANE_Handle device;
+  SANE_Status status = sane_open(this->selected_device->name, &device);
+  if(!this->isGoood(status)) {
+    this->log("Error trying to open device", status);
+    return 1;
+  }
+  if(device) {
+    status = sane_start(device);
+    if(!this->isGoood(status)) {
+      this->log("Error trying to start scanner", status);
+        return 1;
+    }
+    
+  }else{
+    this->log("An error ocurr while trying to open the device!");
+    return 1;
+  }
   return 0;
 }
 
